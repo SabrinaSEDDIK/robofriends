a1d1ce6ac496dc166447bc881cdf908110f1da4c
SabrinaSEDDIK <sabrina.seddik@ymail.com>
2022-03-17 07:04:46 +0100

    starting

---
+++ src/components/Card.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+class Card extends Component{
+    render(){
+        return(
+            <div>
+                
+            </div>
+        );
+    }
+}
