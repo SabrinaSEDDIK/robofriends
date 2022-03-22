db738da6a3877d9c0c4d699175b010376aa99ea2
SabrinaSEDDIK <sabrina.seddik@ymail.com>
2022-03-22 07:21:04 +0100

    invisible scroll

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -1,7 +1,18 @@
+// this comment tells babel to convert jsx to calls to a function called jsx instead of React.createElement
+/** @jsxImportSource @emotion/react */
+import { css, jsx } from '@emotion/react'
 
 const Scroll = ({children}) => {
     return(
-        <div style={{overflowY: 'scroll', border: '3px solid black', height: '800px'}}>
+        <div css={css`
+        overflow-y: scroll; 
+        border: 3px solid black; 
+        height: 800px; 
+        scrollbar-width: none;
+        ::-webkit-scrollbar {
+            width: 0px;
+            background: transparent; /* Chrome/Safari/Webkit */
+        }` }>
             {children}
         </div>
     );
